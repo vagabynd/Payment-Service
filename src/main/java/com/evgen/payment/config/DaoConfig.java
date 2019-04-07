5056953c76d98a43943806610b8f918e141035b7
Yauheni Rotar <Yauheni.Rotar@ihg.com>
2019-04-07 18:00:36 +0300

    add mongoDb

--- src/main/java/com/evgen/payment/config/DaoConfig.java
+++ src/main/java/com/evgen/payment/config/DaoConfig.java
@@ -1,18 +1,31 @@
 package com.evgen.payment.config;
 
+import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 
 @Configuration
-public class DaoConfig {
+@EnableMongoRepositories(basePackages = "com.evgen.payment.repository")
+public class DaoConfig extends AbstractMongoConfiguration {
+
+  @Value("${mongo.url}")
+  private String url;
+
+  @Value("${mongo.name}")
+  private String name;
 
   @Bean
   RestTemplate restTemplate(ObjectMapper objectMapper) {
@@ -28,6 +41,22 @@ public class DaoConfig {
     restTemplate.setMessageConverters(Collections.singletonList(mappingJackson2HttpMessageConverter));
 
     return restTemplate;
+  }
+
+  @Override
+  protected String getDatabaseName() {
+    return name;
+  }
+
+  @Override
+  public MongoClient mongoClient() {
+    MongoClientURI uri = new MongoClientURI(url);
+    return new MongoClient(uri);
+  }
+
+  @Override
+  protected Collection<String> getMappingBasePackages() {
+    return Collections.singleton("com.evgen");
   }
 
 }
